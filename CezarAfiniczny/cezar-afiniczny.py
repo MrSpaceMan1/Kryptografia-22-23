502bc56443693c90a48c21df6abbc3b859925fcb
Bartłomiej Wujec <b.wujec.444@studms.ug.edu.pl>
2022-05-02 08:44:51 +0200

    Dodano rozdzielenie pomiędzy wielkimi i małymi literami, oraz pomijanie wszystkich innych znaków które nie są literami

--- CezarAfiniczny/cezar-afiniczny.py
+++ CezarAfiniczny/cezar-afiniczny.py
@@ -2,6 +2,16 @@ import sys
 from abc import ABC, abstractmethod
 
 
+def is_lowercase_letter(letter: str):
+    letter = ord(letter)
+    return ord('a') <= letter <= ord('z')
+
+
+def is_uppercase_letter(letter: str):
+    letter = ord(letter)
+    return ord('A') <= letter <= ord('Z')
+
+
 class CryptoMethod(ABC):
     @abstractmethod
     def compare_sign(self, sign: str) -> bool:
@@ -37,10 +47,13 @@ class Affine(CryptoMethod):
         a, b = key
         encrypted = ""
         for i in line:
-            if i == " ":
-                encrypted += " "
-                continue
-            encrypted += chr(((((ord(i) - ord('A')) * a + b) % 26) + ord('A')))
+            if is_lowercase_letter(i):
+                encrypted += chr(((((ord(i) - ord('a')) * a + b) % 26) + ord('a')))
+            elif is_uppercase_letter(i):
+                encrypted += chr(((((ord(i) - ord('A')) * a + b) % 26) + ord('A')))
+            else:
+                encrypted += i
+
         return encrypted
 
     def decrypt(self, line, key):
@@ -50,17 +63,21 @@ class Affine(CryptoMethod):
             return ""
         decrypted = ""
         for i in line:
-            if i == " ":
-                decrypted += " "
-                continue
-            decrypted += chr(((ord(i) - ord('A') - b) * a % 26) + ord('A'))
+            if is_lowercase_letter(i):
+                decrypted += chr(((((ord(i) - ord('a') - b) * a) % 26) + ord('a')))
+            elif is_uppercase_letter(i):
+                decrypted += chr(((((ord(i) - ord('A') - b) * a) % 26) + ord('A')))
+            else:
+                decrypted += i
         return decrypted
 
     def plaintext_cryptoanalysis(self, encrypted, plain):
         y1, y2 = encrypted[:2]
         x1, x2 = plain
-        y1, y2 = ord(y1) - ord('A'), ord(y2) - ord('A')
-        x1, x2 = ord(x1) - ord('A'), ord(x2) - ord('A')
+        y1 = ord(y1) - ord('A') if is_uppercase_letter(y1) else ord(y1) - ord('a')
+        y2 = ord(y2) - ord('A') if is_uppercase_letter(y2) else ord(y2) - ord('a')
+        x1 = ord(x1) - ord('A') if is_uppercase_letter(x1) else ord(x1) - ord('a')
+        x2 = ord(x2) - ord('A') if is_uppercase_letter(x2) else ord(x2) - ord('a')
         try:
             a = int(((y2 - y1) / (x1 - x2))**-1 % 26)
             for i in range(1, 26):
@@ -92,10 +109,13 @@ class Caesar(CryptoMethod):
 
         encrypted = ""
         for i in line:
-            if i == " ":
-                encrypted += " "
-                continue
-            encrypted += chr((((ord(i) - ord('A') + key) % 26) + ord('A')))
+            if is_lowercase_letter(i):
+                encrypted += chr((((ord(i) - ord('a') + key) % 26) + ord('a')))
+            elif is_uppercase_letter(i):
+                encrypted += chr((((ord(i) - ord('A') + key) % 26) + ord('A')))
+            else:
+                encrypted += i
+
         return encrypted
 
     def decrypt(self, line, key):
@@ -106,15 +126,17 @@ class Caesar(CryptoMethod):
 
         decrypted = ""
         for i in line:
-            if i == " ":
-                decrypted += " "
-                continue
-            decrypted += chr((((ord(i) - ord('A') - key) % 26) + ord('A')))
+            if is_lowercase_letter(i):
+                decrypted += chr((((ord(i) - ord('a') - key) % 26) + ord('a')))
+            elif is_uppercase_letter(i):
+                decrypted += chr((((ord(i) - ord('A') - key) % 26) + ord('A')))
+            else:
+                decrypted += i
         return decrypted
 
     def plaintext_cryptoanalysis(self, encrypted, plain):
-        en = ord(encrypted[0]) - ord('A')
-        pl = ord(plain[0]) - ord('A')
+        en = ord(encrypted[0]) - ord('A') if is_uppercase_letter(encrypted[0]) else ord(encrypted[0]) - ord('a')
+        pl = ord(plain[0]) - ord('A') if is_uppercase_letter(encrypted[0]) else ord(encrypted[0]) - ord('a')
         for i in range(1, 25):
             if (en - i) % 26 == pl:
                 return i
@@ -124,10 +146,12 @@ class Caesar(CryptoMethod):
         decrypted_lines = ""
         for i in range(1, 26):
             for j in encrypted:
-                if j == " ":
-                    decrypted_lines += " "
-                    continue
-                decrypted_lines += chr((ord(j) - ord('A') - i) % 26 + ord('A'))
+                if is_lowercase_letter(j):
+                    decrypted_lines += chr((((ord(j) - ord('a') - i) % 26) + ord('a')))
+                elif is_uppercase_letter(j):
+                    decrypted_lines += chr((((ord(j) - ord('A') - i) % 26) + ord('A')))
+                else:
+                    decrypted_lines += j
             decrypted_lines += '\n'
         return decrypted_lines
 
@@ -150,7 +174,7 @@ class CryptographicTool:
                 key = None
 
                 with open('./plain.txt', 'r') as plain:
-                    line_to_encrypt = plain.readline().upper()
+                    line_to_encrypt = plain.readline()
                     plain.close()
 
                 with open('./key.txt', 'r') as key_file:
@@ -173,7 +197,7 @@ class CryptographicTool:
                 key = None
 
                 with open('./encrypted.txt', 'r') as encrypted:
-                    line_to_encrypt = encrypted.readline().upper()
+                    line_to_encrypt = encrypted.readline()
                     encrypted.close()
 
                 with open('./key.txt', 'r') as key_file:
@@ -200,14 +224,17 @@ class CryptographicTool:
                     encrypted.close()
 
                 with open('./extra.txt', 'r') as extra:
-                    plaintext_line = extra.readline().upper()
+                    plaintext_line = extra.readline()
                     extra.close()
 
                 key = i.plaintext_cryptoanalysis(encrypted_line, plaintext_line)
                 tmp = ""
-                for i in key:
-                    tmp+=f"{i} "
-                key = tmp
+
+                if type(key) is int or type(key) is tuple:
+                    for i in key:
+                        tmp+=f"{i} "
+                    key = tmp
+
                 with open('./key-found.txt', 'w') as key_found:
                     key_found.write(str(key))
                     key_found.close()
